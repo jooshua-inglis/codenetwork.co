7d72b019e3f086c24adca77b1d619874aa8b282a
Joshua Inglis <jt.inglis@outlook.com>
2020-03-12 19:55:57 +1000

    removed bug from index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,12 +24,6 @@ const IndexPage = () => {
     }
   `).allExecsYaml.nodes
 
-  const Bug = styled.div`
-    color: red;
-    img {
-      width: 100px;
-    }
-  `
 
   return (
     <Layout>
@@ -42,10 +36,6 @@ const IndexPage = () => {
         talent in the world, starting here in Brisbane. We’re based at QUT and
         are expanding to other universities and organisations in the area.
       </p>
-      <Bug>
-        CODE NETWORK BASICS BUG
-        <img src="https://i0.wp.com/entomologytoday.org/wp-content/uploads/2019/01/brown-marmorated-stink-bug-Halyomorpha-halys-close-up.jpg?resize=878%2C515&ssl=1" />
-      </Bug>
       <p>
         This site is a work in progress. If you'd like to contribute, you can do
         so <a href="https://github.com/codenetwork/codenetwork.co">here</a>
