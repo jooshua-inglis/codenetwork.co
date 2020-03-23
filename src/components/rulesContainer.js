d4ae51b82eaf841cf0fa237e81c00f9a09d9d40c
Joshua Inglis <jt.inglis@outlook.com>
2020-03-24 01:07:49 +1000

    add rules page (#28)

---
+++ src/components/rulesContainer.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const RulesContainer = styled.div`
+  text-align: justify;
+  h3 {
+    margin-bottom: 10px;
+    margin-left: 0;
+  }
+  .summary {
+    margin-left: 10px;
+    font-size: 10pt;
+    line-height: 13pt;
+    font-style: italic;
+  }
+`
+export default RulesContainer
